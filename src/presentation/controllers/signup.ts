eb10caf4a27196ba8a9b8ef434d61cbf48c26d4b
Caio F <caiofsousa27@hotmail.com>
2022-08-15 13:09:44 -0300

    feat: aula 7

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,16 @@
-export class SignUpController {
-  handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-    };
+import { httpRequest, httpResponse } from "../protocols/http";
+import { MissingParamError } from "../errors/missing-param-error";
+import { badRequest } from "../helpers/http-helper";
+import { Controller } from "../protocols/controller";
+
+export class SignUpController implements Controller {
+  handle(httpRequest: httpRequest): httpResponse {
+    const requireFields = ["name", "email", "password", "passwordConfirmation"];
+
+    for (const field of requireFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
   }
 }
