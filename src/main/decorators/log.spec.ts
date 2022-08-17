d15a1f0132f6380a0ed4aa12a9ffeaafc260841a
Caio F <caiofsousa27@hotmail.com>
2022-08-17 13:57:14 -0300

    refactor: 22 23

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,70 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols";
+import { logControllerDecorator } from "./log";
+
+interface sutTypes {
+  sut: logControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: "Caio",
+        },
+      };
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): sutTypes => {
+  const controllerStub = makeController();
+  const sut = new logControllerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub,
+  };
+};
+
+describe("LogController Decorator", () => {
+  test("Should call controller handle", async () => {
+    const { sut, controllerStub } = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, "handle");
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+  test("Should return the same result of the controller", async () => {
+    const { sut, controllerStub } = makeSut();
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual({
+        statusCode: 200,
+        body: {
+          name: "Caio",
+        },
+      });
+  });
+});
---
+++ src/main/decorators/log.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols";
+
+export class logControllerDecorator implements Controller {
+  private readonly controller: Controller;
+  constructor(controller: Controller) {
+    this.controller = controller;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest);
+    return httpResponse;
+    /*const httpResponse = await this.controller.handle(httpRequest);
+    if (httpResponse.statusCode === 500) {
+      //log
+    }
+    return httpResponse;*/
+  }
+}
